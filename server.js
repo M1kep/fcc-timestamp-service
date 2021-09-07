a6335461078799f3cc3a2ac5a2edfc5bd3c73485
Michael Poutre <m1kep.my.mail@gmail.com>
2021-09-06 21:58:31 -0700

    Upload working Timestamp Service Example

--- server.js
+++ server.js
@@ -2,8 +2,10 @@
 // where your node app starts
 
 // init project
+require('dotenv').config();
 var express = require('express');
 var app = express();
+const {dateHandler} = require("./lib/handlers/api-date-handler");
 
 // enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
 // so that your API is remotely testable by FCC 
@@ -20,13 +22,16 @@ app.get("/", function (req, res) {
 
 
 // your first API endpoint... 
-app.get("/api/hello", function (req, res) {
-  res.json({greeting: 'hello API'});
-});
-
-
+app.get('/api/:date', dateHandler)
+app.get('/api', (req, res) => {
+    const date = new Date(Date.now());
+    res.json({
+        unix: date.getTime(),
+        utc: date.toUTCString()
+    })
+})
 
 // listen for requests :)
-var listener = app.listen(process.env.PORT, function () {
+var listener = app.listen(process.env.PORT || 3000, function () {
   console.log('Your app is listening on port ' + listener.address().port);
 });
